9109a22ca2005e8b7d7ce6067f3b081c68874c60
sehmusaydogdu <sehmusaydogdu47@gmail.com>
2017-11-27 22:35:57 +0300

    Initial commit

---
+++ app/src/main/java/com/text_audio_converter/MainActivity.java
@@ -0,0 +1,58 @@
+package com.text_audio_converter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.Locale;
+
+public class MainActivity extends Activity {
+
+
+    private TextToSpeech tts;
+    private boolean flag;
+    private EditText txtMetin;
+    private Button btn;
+
+    private void init(){
+        txtMetin=(EditText)findViewById(R.id.txtMetin);
+        btn=(Button)findViewById(R.id.btn);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+
+        tts=new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != TextToSpeech.ERROR) {
+                    //Locale locale = new Locale("tr", "TR");
+                    tts.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String toSpeak = txtMetin.getText().toString();
+                //Toast.makeText(getApplicationContext(), toSpeak,Toast.LENGTH_SHORT).show();
+                tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    public void onPause(){
+        if(tts !=null){
+            tts.stop();
+            tts.shutdown();
+        }
+        super.onPause();
+    }
+}
